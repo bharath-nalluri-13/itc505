1069b6201732fd32c8e6520dd46307c4e9c475c2
bharath-nalluri-13 <113639532+bharath-nalluri-13@users.noreply.github.com>
2023-11-27 18:39:22 -0700

    Create server.js

---
+++ functions/server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+// Middleware to parse the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+// POST route handler for the mad lib form
+app.post("/itc505/lab7", (req, res) => {
+  // Extract data from the form
+  const { noun, adjective, verb, pluralNoun, adverb } = req.body;
+
+  // Your Mad Lib template
+  const madLib = `Grab your ${adjective} ${noun} and ${adverb} ${verb} to NAU University. Explore the ${adjective} SBS building, and the ${adjective} engineering building. Head to the HLC for ${adjective} games, and other cool ${pluralNoun}.`;
+
+  res.send(madLib);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
